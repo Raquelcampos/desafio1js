f37941892280cdc5ab36564d6e30ed279703cc26
Raquelcampos <martinsraquelcampos@gmail.com>
2022-06-10 09:49:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let nome = "Raquel";
+console.log(nome);
+let numero = 22;
+let ano = 2022;
+console.log(ano-numero);
+console.log("Estou mexendo no console.log");
+let quartaFeira = false;
+console.log(quartaFeira);
+console.log(typeof quartaFeira);
